db97c58a48cf77b630179e86c7b00719552e5635
onderonur <onuronder.onr@gmail.com>
2024-09-13 15:27:30 +0300

    refactor 🚀

--- src/components/todo-form.tsx
+++ src/components/todo-form.tsx
@@ -4,21 +4,27 @@ import { addTodo } from "@/lib/actions";
 import { Input } from "./ui/input";
 import { SubmitButton } from "./submit-button";
 import { useRef } from "react";
+import { Label } from "./ui/label";
 
 export function TodoForm() {
   const formRef = useRef<React.ElementRef<"form">>(null);
 
   return (
     <form
-      className="flex flex-col gap-1 items-end"
+      className="flex flex-col gap-2"
       ref={formRef}
       action={async (formData) => {
         await addTodo(formData);
         formRef.current?.reset();
       }}
     >
-      <Input name="title" />
-      <SubmitButton>Save</SubmitButton>
+      <div>
+        <Label htmlFor="title">Todo Title</Label>
+        <Input id="title" name="title" placeholder="Add a todo..." />
+      </div>
+      <div className="flex justify-end">
+        <SubmitButton type="button">Submit</SubmitButton>
+      </div>
     </form>
   );
 }
